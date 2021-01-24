0a15fa94065c25f55d3836ae516bab21aa84eb2a
Wei Bin <chiuweibin.dev@gmail.com>
2021-01-24 22:06:08 +0800

    Initial commit

---
+++ src/test/java/companyname/TestLogin.java
@@ -0,0 +1,39 @@
+//filename: tests/TestLogin.java
+package companyname;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestLogin {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chiu\\chromedriver_win32\\chromedriver.exe");
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setCapability("browserVersion", "86.0");
+        driver = new ChromeDriver(browserOptions);
+    }
+
+    @Test
+    public void succeeded() {
+        driver.get("http://the-internet.herokuapp.com/login");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector("button")).click();
+        assertTrue("success message not present",
+                driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
