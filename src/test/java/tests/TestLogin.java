24fe4954e8a08b6ec40efe7438a62d484ae4bdc0
Wei Bin <chiuweibin.dev@gmail.com>
2021-02-03 09:32:39 +0800

    Implement page object model

--- src/test/java/tests/TestLogin.java
+++ src/test/java/tests/TestLogin.java
@@ -5,14 +5,19 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
-import org.openqa.selenium.By;
+//import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pageobjects.Login;
+import static org.junit.Assert.assertTrue;
+
+
 
 public class TestLogin {
 
     private WebDriver driver;
+    private Login login;
 
     @Before
     public void setUp() {
@@ -20,16 +25,28 @@ public class TestLogin {
         ChromeOptions browserOptions = new ChromeOptions();
         browserOptions.setCapability("browserVersion", "88.0");
         driver = new ChromeDriver(browserOptions);
+        login = new Login(driver);
     }
 
     @Test
     public void succeeded() {
-        driver.get("http://the-internet.herokuapp.com/login");
-        driver.findElement(By.id("username")).sendKeys("tomsmith");
-        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-        driver.findElement(By.cssSelector("button")).click();
+//        driver.get("http://the-internet.herokuapp.com/login");
+//        driver.findElement(By.id("username")).sendKeys("tomsmith");
+//        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+//        driver.findElement(By.cssSelector("button")).click();
+//        assertTrue("success message not present",
+//                driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
+        login.with("tomsmith", "SuperSecretPassword!");
         assertTrue("success message not present",
-                driver.findElement(By.cssSelector(".flash.success")).isDisplayed());
+                login.successMessagePresent());
+
+    }
+
+    @Test
+    public void failed() {
+        login.with("tomsmith", "bad password");
+        assertTrue("failure message wasn't present after providing bogus credentials",
+                login.failureMessagePresent());
     }
 
     @After
